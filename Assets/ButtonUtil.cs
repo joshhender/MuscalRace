dd336043f7d987bdf7b4633ceb759a8d27b8bd7e
GlitchBoss <theprograminator@gmail.com>
2015-09-04 10:06:36 -0400

    Created Game

---
+++ Assets/ButtonUtil.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonUtil : MonoBehaviour {
+
+	public void StartGame()
+	{
+		GameManager.instance.StartGame ();
+	}
+
+	public void GoUpOne()
+	{
+		GameManager.instance.ChangeCurrentNote (true);
+	}
+
+	public void GoDownOne()
+	{
+		GameManager.instance.ChangeCurrentNote (false);
+	}
+
+	public void CheckAnswer()
+	{
+		GameManager.instance.CheckAnswer ();
+	}
+}
